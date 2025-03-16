40f31b2e6b8757195c033e136e1c199cb054b274
Liz Shav <nxchavarria@gmail.com>
2025-03-16 14:16:31 -0600

    implementar funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 let amigos = [];
+function agregarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.value.trim();
+
+    // validar que el campo no este vacio 
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    // validar que el nombre no este duplicado
+    if (amigos.includes(nombreAmigo)) {//compruebe si el nombre ya existe en el array 'amigos'
+        alert(`El nombre "${nombreAmigo}" ya esta en la lista de amigos`);//muestra una alerta indicando que el nombre esta duplicado
+        return;
+
+    }
+       //agregar el nombre a la lista 
+       amigos.push(nombreAmigo);
+
+       //Limpiar el campo de entrada para que este vacio
+       inputAmigo.Value = "";
+   
+       //actualizar la lista en el HTML
+       actualizarLista();
+   
+}
